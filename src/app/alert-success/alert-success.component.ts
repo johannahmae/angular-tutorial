a1bf56262f20c0c78829fc5e7b9fa4798984fe73
johannahA <johannah.mae.abestano@gmail.com>
2017-08-24 01:16:44 +0800

    feat(app): using #id, @Output and @Input directives for components.

--- src/app/alert-success/alert-success.component.ts
+++ src/app/alert-success/alert-success.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+
+export interface ClosedEvent {
+  name: string;
+  timestamp: Date;
+  duration: number;
+  totalDuration: number;
+}
 
 @Component({
   selector: 'app-alert-success',
@@ -6,8 +13,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alert-success.component.css']
 })
 export class AlertSuccessComponent implements OnInit {
+  initialShow: Date;
+  lastShow: Date;
+  visible: boolean;
+  @Input() message: string;
+  @Output() closed: EventEmitter<ClosedEvent> = new EventEmitter();
 
-  constructor() { }
+  constructor() {
+    this.message = '';
+    this.visible = false;
+  }
+
+  async show(timeout = 5000) {
+    console.log(this);
+    this.lastShow = new Date();
+    if (!this.visible) {
+      this.initialShow = this.lastShow;
+      this.visible = true;
+      while ((new Date).valueOf() - this.lastShow.valueOf() < timeout) {
+        await new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve();
+          }, 100);
+        });
+      }
+      this.visible = false;
+      this.closed.emit({
+        name: `some event ${this.message}`,
+        timestamp: new Date(),
+        duration: (new Date).valueOf() - this.lastShow.valueOf(),
+        totalDuration: (new Date).valueOf() - this.initialShow.valueOf(),
+      });
+    }
+  }
+
+  getOutput(): string {
+    return this.message ? this.message : 'Ok';
+  }
 
   ngOnInit() {
   }
--- src/app/assignment-incrementing-log/assignment-incrementing-log.component.ts
+++ src/app/assignment-incrementing-log/assignment-incrementing-log.component.ts
@@ -1,13 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {AlertSuccessComponent, ClosedEvent} from '../alert-success/alert-success.component';
 
 interface Blob {
   id: number;
   content: string;
   timestamp: Date;
-}
-
-interface Blobs {
-  [index: number]: Blob;
 }
 
 @Component({
@@ -21,17 +18,26 @@ export class AssignmentIncrementingLogComponent implements OnInit {
   content: string;
   blobs: Array<Blob> = [];
 
-  onSubmit() {
+  message: string;
+
+  async onSubmit(appAlert: AlertSuccessComponent) {
     this.blobs.push({
       content: this.content,
       id: this.count++,
       timestamp: new Date()
     });
+    this.message = `added ${this.content}`;
     this.content = '';
+    await appAlert.show(10000);
+  }
+
+  onAppAlertClosed(e: ClosedEvent) {
+    console.log(e);
   }
 
   constructor() {
     this.blobs = [];
+    this.message = '';
   }
 
   ngOnInit() {

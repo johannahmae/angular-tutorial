5dbf40026454eb87f1d11234eece5767f79ba3d8
johannahA <johannah.mae.abestano@gmail.com>
2017-08-24 01:29:14 +0800

    feat(app): using @ViewChild

--- src/app/assignment-incrementing-log/assignment-incrementing-log.component.ts
+++ src/app/assignment-incrementing-log/assignment-incrementing-log.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {AlertSuccessComponent, ClosedEvent} from '../alert-success/alert-success.component';
 
 interface Blob {
@@ -16,11 +16,15 @@ export class AssignmentIncrementingLogComponent implements OnInit {
 
   count = 1;
   content: string;
+  @ViewChild('contentType') contentType: ElementRef;
   blobs: Array<Blob> = [];
 
   message: string;
 
   async onSubmit(appAlert: AlertSuccessComponent) {
+    // accessing value of dom using @ViewChild. do not change value. bad practice.
+    console.log(this.contentType.nativeElement.value);
+
     this.blobs.push({
       content: this.content,
       id: this.count++,
